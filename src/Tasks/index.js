e24e6afe439858044e7ef5153a29855113a61a08
Szymon Węglarz <simon.weglarz@gmail.com>
2021-10-20 18:57:31 +0200

    class renaming from section to tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,18 +1,18 @@
 import "./style.css";
 
 const Tasks = ({tasks, hideDoneTasks}) => (
-    <ul className="section__list">
+    <ul className="tasks__list">
         {tasks.map(task => (
             <li key = {task.id}
-            className = {`section__item ${task.done && hideDoneTasks ? "section__item--hidden" : ""}`}
+            className = {`tasks__item ${task.done && hideDoneTasks ? "tasks__item--hidden" : ""}`}
             >
-             <button className = "section__button section__button--done">
+             <button className = "tasks__button tasks__button--done">
             {task.done ? "✔" : ""}
             </button>
             
-            <span className ={`section__span
-            ${task.done ? "section__span--done" : ""}`}>{task.content}</span>
-            <button className ={`section__button section__button--remove`}>🗑️</button>
+            <span className ={`tasks__span
+            ${task.done ? "tasks__span--done" : ""}`}>{task.content}</span>
+            <button className ={`tasks__button tasks__button--remove`}>🗑️</button>
             </li>
         ))}
     </ul>
