a7c72dad3b6316d7d62ac4ae76dcd1c298ae7772
sweglarz <67336019+sweglarz@users.noreply.github.com>
2021-11-23 12:54:38 +0100

    deleted BorderlessHeader styles

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -4,7 +4,7 @@ export const SectionElement = styled.section`
     background-color: #fff;
     padding: 5px 0;
     margin: 10px;
-`
+`;
 
 export const HeaderBox = styled.div`
     display: flex;
@@ -15,16 +15,11 @@ export const HeaderBox = styled.div`
     @media(max-width: 500px){
         display: flex;
         flex-direction: column;
-    }
-`
+    };
+`;
 
 export const SectionHeader = styled.h2`
     margin: 0px;
     padding: 10px;
-    border-bottom: 2px solid #eee;
     font-size: 20px;
-`
-
-export const BorderlessHeader = styled(SectionHeader)`
-    border-bottom: none;
-`
+`;
